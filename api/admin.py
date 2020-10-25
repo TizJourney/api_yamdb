8841fbf864a3b9ee735b46b0f749e62b7ab0596b
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-25 17:47:23 +0300

    feat: добавить редактирование пользователя в админке

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import YamDBUserCreationForm, YamDBUserChangeForm
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+class YamDBUserAdmin(UserAdmin):
+    model = User
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name',  'bio', 'role'),
+        }),
+    )
+    fieldsets = UserAdmin.fieldsets +  (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('bio', 'role'),
+        }),
+    )
+    list_display = ('username', 'email', 'bio', 'role')
+
+admin.site.register(User, YamDBUserAdmin)
