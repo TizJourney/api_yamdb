b467ee1d1cacaf3189eb5276bdaa2e972eabf1b3
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-25 18:17:38 +0300

    [feat #17] Сделать базовые эндпоинты для пользователя

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+router = DefaultRouter()
+router.register(r'users', views.UsersViewSet)
 
 v1_url_patterns = [
     # это заглушки для проверки правильности подключения jwt-токенов. Ближайшее время будут убраны.
@@ -13,4 +19,5 @@ v1_url_patterns = [
 
 urlpatterns = [
     path('v1/', include(v1_url_patterns)),
+    path('v1/', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .serializers import UserSerializer
 
-# Create your views here.
+
+User = get_user_model()
+
+
+class UsersViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
