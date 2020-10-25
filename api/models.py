8567a2510577efa7c350fb8064934880f7fedd12
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-25 17:46:44 +0300

    feat: добавить модель пользователя

--- api/models.py
+++ api/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+ROLE_CHOICES = (
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+)
+ROLE_MAX_LENGTH = max((len(r[0]) for r in ROLE_CHOICES))
+
+
+class YamDBUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    bio = models.TextField(blank=True)
+    role = models.CharField(
+        max_length=ROLE_MAX_LENGTH,
+        choices=ROLE_CHOICES,
+        default='user'
+    )
