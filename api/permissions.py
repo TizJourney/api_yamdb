8255c491e1a8948bc235e8fd7392680f47e5a694
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-29 15:41:49 +0300

    feat: разрешить читать пользователей только администраторам и суперпользователям

--- api/permissions.py
+++ api/permissions.py
@@ -4,10 +4,9 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class IsAdminOrReadOnly(permissions.BasePermission):
+class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
-            request.method in permissions.SAFE_METHODS or
             request.user.is_superuser or
             request.user.role == User.Role.ADMIN
         )
