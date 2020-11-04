8308b1e4ea43589abd40262a7ee523c8add47d39
Konstantin Bondar <kostya.bondar@gmail.com>
2020-11-04 23:58:24 +0300

    feat: поправить тесты

--- api/permissions.py
+++ api/permissions.py
@@ -20,9 +20,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method == 'GET' or
-                request.user.is_superuser or
+            request.method == 'GET' or
+            request.user.is_superuser or
+            (
+                request.user.is_authenticated and
                 request.user.role == User.Role.ADMIN
+            )
         )
 
 
