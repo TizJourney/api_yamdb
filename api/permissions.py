39d2963d77cd05a13256aa5c1aa0fbd9c5ffd0a7
Konstantin Bondar <kostya.bondar@gmail.com>
2020-11-05 11:16:43 +0300

    Revert "Merge branch 'develop'"
    This reverts commit 61bf458e8ac037ef78091adb27158dfc8b707b9c, reversing
    changes made to df6e97fcf739be8670798671a056550508fdfdd3.

--- api/permissions.py
+++ api/permissions.py
@@ -20,12 +20,9 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-            request.method == 'GET' or
-            request.user.is_superuser or
-            (
-                request.user.is_authenticated and
+                request.method == 'GET' or
+                request.user.is_superuser or
                 request.user.role == User.Role.ADMIN
-            )
         )
 
 
